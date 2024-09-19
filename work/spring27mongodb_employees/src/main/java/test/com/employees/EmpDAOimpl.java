4c3b3d467ff0aff022d295403bb0af128a973100
yang <tkwkdlq@gmail.com>
2024-09-19 19:13:41 +0900

    과제 못했던거 수정

--- work/spring27mongodb_employees/src/main/java/test/com/employees/EmpDAOimpl.java
+++ work/spring27mongodb_employees/src/main/java/test/com/employees/EmpDAOimpl.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -21,20 +23,65 @@ public class EmpDAOimpl implements EmpDAO {
 
 	@Override
 	public long insertOne(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		log.info("insertOne()...");
+		log.info("vo:{}",vo);
+		
+		long flag = 0;
+		
+		try {
+			Document doc = new Document();
+			doc.put("employee_id", vo.getEmployee_id());
+			doc.put("first_name", vo.getFirst_name());
+			doc.put("last_name", vo.getLast_name());
+			doc.put("email", vo.getEmail());
+			
+			employees.insertOne(doc);
+			
+			flag = 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
 	}
 
 	@Override
 	public long updateOne(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		log.info("updateOne()...");
+		log.info("vo:{}",vo);
+		
+		long flag = 0;
+		
+		try {
+			Document doc = new Document();
+			doc.put("first_name", vo.getFirst_name());
+			doc.put("last_name", vo.getLast_name());
+			doc.put("email", vo.getEmail());
+			
+			Bson set = new Document("$set",doc);
+			
+			flag = employees.updateOne(new Document("employee_id",vo.getEmployee_id()), set).getModifiedCount();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
 	}
 
 	@Override
 	public long deleteOne(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		log.info("deleteOne()...");
+		log.info("vo:{}",vo);
+		
+		long flag = 0;
+		
+		try {
+
+			flag = employees.deleteOne(new Document("employee_id",vo.getEmployee_id())).getDeletedCount();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
 	}
 
 	@Override
@@ -60,14 +107,49 @@ public class EmpDAOimpl implements EmpDAO {
 
 	@Override
 	public List<EmpVO> searchList(String searchKey, String searchWord, int page, int limit) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("searchList()...");
+		log.info("searchKey:{}",searchKey);
+		log.info("searchWord:{}",searchWord);
+		log.info("page:{}",page);
+		log.info("limit:{}",limit);
+		
+		Bson filter = Filters.regex(searchKey, searchWord);
+		
+		FindIterable<Document> docs = employees.find(filter)
+				.sort(new Document("employee_id",-1)).skip((page-1)*limit).limit(limit);
+		
+		List<EmpVO> list = new ArrayList<EmpVO>();
+		for(Document doc : docs) {
+			EmpVO vo = new EmpVO();
+			vo.setMid(doc.get("_id").toString());
+			vo.set_id(doc.get("_id").toString());
+			vo.setEmployee_id(doc.getInteger("employee_id", 0));
+			vo.setFirst_name(doc.getString("first_name"));
+			vo.setLast_name(doc.getString("last_name"));
+			vo.setEmail(doc.getString("email"));
+			list.add(vo);
+		}
+		return list;
 	}
 
 	@Override
 	public EmpVO findOne(EmpVO vo) {
-		// TODO Auto-generated method stub
-		return null;
+		log.info("findOne()...");
+		log.info("vo:{}",vo);
+
+		FindIterable<Document> docs = 
+				employees.find(new Document("employee_id",vo.getEmployee_id())).sort(new Document("employee_id",-1));
+
+		EmpVO vo2 = new EmpVO();
+		for(Document doc : docs) {
+			vo2.setMid(doc.get("_id").toString());
+			vo2.set_id(doc.get("_id").toString());
+			vo2.setEmployee_id(doc.getInteger("employee_id", 0));
+			vo2.setFirst_name(doc.getString("first_name"));
+			vo2.setLast_name(doc.getString("last_name"));
+			vo2.setEmail(doc.getString("email"));
+		}
+		return vo2;
 	}
 
 }
--- work/spring27mongodb_employees/src/main/java/test/com/employees/EmpRestController.java
+++ work/spring27mongodb_employees/src/main/java/test/com/employees/EmpRestController.java
@@ -2,8 +2,10 @@ package test.com.employees;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,7 +26,7 @@ public class EmpRestController {
 	@Autowired
 	EmpService service;
 	
-	
+	@ResponseBody
 	@RequestMapping(value = "/findAll.do", method = RequestMethod.GET)
 	public List<EmpVO> findAll(@RequestParam(defaultValue = "0")int page,
 			@RequestParam(defaultValue = "0")int limit) {
@@ -36,4 +39,73 @@ public class EmpRestController {
 		return list;
 	}
 	
+	@ResponseBody
+	@RequestMapping(value = "/findOne.do", method = RequestMethod.GET)
+	public EmpVO findOne(EmpVO vo) {
+		log.info("/findOne.do");
+		
+		EmpVO vo2 = service.findOne(vo);
+		log.info("vo2:{}",vo2);
+		
+		
+		return vo2;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/searchList.do", method = RequestMethod.GET)
+	public List<EmpVO> searchList(@RequestParam(defaultValue = "first_name")String searchKey,
+			@RequestParam(defaultValue = "K")String searchWord,
+			@RequestParam(defaultValue = "0")int page,
+			@RequestParam(defaultValue = "0")int limit) {
+		log.info("/searchList.do");
+		log.info("searchKey:{}",searchKey);
+		log.info("searchWord:{}",searchWord);
+		log.info("page:{}",page);
+		log.info("limit:{}",limit);
+		
+		List<EmpVO> list = service.searchList(searchKey,searchWord,page,limit);
+		log.info("list.size():{}",list.size());
+		
+		
+		return list;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/insertOneOK.do", method = RequestMethod.GET)
+	public Map<String,Long> insertOneOK(EmpVO vo) {
+		log.info("/insertOneOK.do");
+		log.info("vo:{}",vo);
+		
+		long result = service.insertOne(vo);
+		Map<String,Long> map = new HashMap<String, Long>();
+		map.put("result", result);
+			
+		return map;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/updateOneOK.do", method = RequestMethod.GET)
+	public Map<String,Long> updateOneOK(EmpVO vo) {
+		log.info("/updateOneOK.do");
+		log.info("vo:{}",vo);
+		
+		long result = service.updateOne(vo);
+		Map<String,Long> map = new HashMap<String, Long>();
+		map.put("result", result);
+			
+		return map;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/deleteOneOK.do", method = RequestMethod.GET)
+	public Map<String,Long> deleteOneOK(EmpVO vo) {
+		log.info("/deleteOneOK.do");
+		log.info("vo:{}",vo);
+		
+		long result = service.deleteOne(vo);
+		Map<String,Long> map = new HashMap<String, Long>();
+		map.put("result", result);
+			
+		return map;
+	}
 }
